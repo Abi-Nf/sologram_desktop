33e1c13e08761f97619011101a289ec887bcd115
Abi-Nf <hei.abigail.3@gmail.com>
2024-08-05 23:34:04 +0300

    chore: add thread utils

--- src/main/java/music/sologram/app/utils/Utils.java
+++ src/main/java/music/sologram/app/utils/Utils.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.net.URL;
 
 import static java.util.Objects.requireNonNull;
+import static javafx.application.Platform.runLater;
 
 public class Utils {
   public static URL loadResource(String name){
@@ -15,6 +16,14 @@ public class Utils {
 
   public static String loadCssFile(String name){
     return loadResource(name).toExternalForm();
+  }
+
+  public static void executeInNewThread(Runnable runnable){
+    new Thread(runnable).start();
+  }
+
+  public static void asyncUiThread(Runnable runnable){
+    runLater(runnable);
   }
 
   public static boolean pathExists(String path){
