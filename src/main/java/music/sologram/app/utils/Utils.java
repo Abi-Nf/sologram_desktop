2343bb9a55a8fb03a57803916b05a9024689d8da
Abi-Nf <hei.abigail.3@gmail.com>
2024-08-05 23:32:40 +0300

    chore: add pathExists(String)

--- src/main/java/music/sologram/app/utils/Utils.java
+++ src/main/java/music/sologram/app/utils/Utils.java
@@ -1,5 +1,6 @@
 package music.sologram.app.utils;
 
+import java.io.File;
 import java.net.URL;
 
 import static java.util.Objects.requireNonNull;
@@ -14,5 +15,9 @@ public class Utils {
 
   public static String loadCssFile(String name){
     return loadResource(name).toExternalForm();
+  }
+
+  public static boolean pathExists(String path){
+    return new File(path).exists();
   }
 }
