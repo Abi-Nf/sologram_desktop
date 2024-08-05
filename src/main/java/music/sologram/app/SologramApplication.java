3197ab75664e8971f0bd2711c44e773f0ad9ead9
Abi-Nf <hei.abigail.3@gmail.com>
2024-08-06 00:08:51 +0300

    chore: checking + splash screen or installation

--- src/main/java/music/sologram/app/SologramApplication.java
+++ src/main/java/music/sologram/app/SologramApplication.java
@@ -2,17 +2,47 @@ package music.sologram.app;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import music.sologram.app.data.HibernateConfig;
+import music.sologram.app.windows.installation.InstallationWindow;
+import music.sologram.app.windows.splash.SplashScreen;
+
+import static music.sologram.app.settings.SettingChecker.isHomeAppOk;
+import static music.sologram.app.settings.SettingChecker.shouldInstallApp;
 
 public class SologramApplication extends Application {
   public static void main(String[] args) {
     launch();
   }
 
+  private SplashScreen splash;
+
+  public SologramApplication() {
+    this.splash = new SplashScreen();
+    if(isHomeAppOk()) splash.show();
+  }
+
+  private void removeSplash() {
+    if(splash != null){
+      splash.close();
+      this.splash = null;
+    }
+  }
+
   @Override
   public void start(Stage stage){
-    stage.setMinWidth(600);
-    stage.setMinHeight(500);
-    stage.centerOnScreen();
-    stage.show();
+    Runnable runnableStage = () -> {
+      HibernateConfig.init();
+      removeSplash();
+      stage.setMinWidth(600);
+      stage.setMinHeight(500);
+      stage.centerOnScreen();
+      stage.show();
+    };
+    if(shouldInstallApp()){
+      var installation = new InstallationWindow(runnableStage);
+      installation.show();
+    }else {
+      runnableStage.run();
+    }
   }
 }
