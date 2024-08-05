8fdbcbe1faeb4007914db9b3327449735ff68636
Abi-Nf <hei.abigail.3@gmail.com>
2024-08-06 00:07:19 +0300

    wip: feat: installation window modal

---
+++ src/main/java/music/sologram/app/windows/installation/InstallationWindow.java
@@ -0,0 +1,9 @@
+package music.sologram.app.windows.installation;
+
+import javafx.stage.Stage;
+
+public class InstallationWindow extends Stage {
+  public InstallationWindow(Runnable afterInstallation) {
+    // TODO: add ui for app installation settings
+  }
+}
