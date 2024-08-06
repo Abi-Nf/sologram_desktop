54c14b2f3080057849e7b07e9d14b491813cf292
Abi-Nf <hei.abigail.3@gmail.com>
2024-08-06 21:44:44 +0300

    chore: ajust splash

--- src/main/java/music/sologram/app/windows/splash/SplashScreen.java
+++ src/main/java/music/sologram/app/windows/splash/SplashScreen.java
@@ -3,12 +3,13 @@ package music.sologram.app.windows.splash;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import static music.sologram.app.utils.Utils.getAppLogo;
 import static music.sologram.app.utils.Utils.loadCssFile;
 import static music.sologram.app.utils.Utils.loadResource;
 
@@ -19,9 +20,10 @@ public class SplashScreen extends Stage {
     centerOnScreen();
     setResizable(false);
     setScene(createScene());
-    setTitle("Sologram splash");
+    setTitle("Splash");
+    getIcons().add(getAppLogo());
+    initModality(Modality.WINDOW_MODAL);
     initStyle(StageStyle.TRANSPARENT);
-    getIcons().add(new Image(loadResource("/icons/logo.png").toExternalForm()));
   }
 
   private Scene createScene(){
