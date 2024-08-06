1f3e95930ba16b3cce12297bd245592a5b494aa5
Abi-Nf <hei.abigail.3@gmail.com>
2024-08-06 21:39:51 +0300

    chore: migrations

--- src/main/java/music/sologram/app/data/HibernateConfig.java
+++ src/main/java/music/sologram/app/data/HibernateConfig.java
@@ -1,18 +1,45 @@
 package music.sologram.app.data;
 
+import music.sologram.app.AppException;
 import music.sologram.app.data.models.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.nio.file.Path;
+
+import static java.nio.file.Files.readString;
+import static java.sql.DriverManager.getConnection;
 import static music.sologram.app.settings.DefaultSettings.getDbPath;
+import static music.sologram.app.utils.Utils.loadResource;
 
 public class HibernateConfig {
   public static SessionFactory SESSION;
 
+  private static String getDbUrl(){
+    return "jdbc:sqlite:" + getDbPath();
+  }
+
+  public static void execMigration(){
+    try {
+      var sql = readString(Path.of(
+        loadResource("/data/migrations.sql").getFile()
+      ));
+      var connection = getConnection(getDbUrl());
+      connection.setAutoCommit(false);
+      var statement = connection.createStatement();
+      statement.execute(sql);
+      connection.commit();
+      connection.setAutoCommit(true);
+      connection.close();
+    } catch (Exception e) {
+      throw new AppException(e);
+    }
+  }
+
   public static void init() {
     var configuration = new Configuration();
-    configuration.setProperty("hibernate.connection.url", "jdbc:sqlite:" + getDbPath());
+    configuration.setProperty("hibernate.connection.url", getDbUrl());
     configuration.setProperty("hibernate.connection.driver_class", "org.sqlite.JDBC");
     configuration.setProperty("hibernate.dialect", "org.hibernate.community.dialect.SQLiteDialect");
     loadEntityClasses(configuration);
