d7856b576972d0b9da775dfe24db686249de99aa
Abi-Nf <hei.abigail.3@gmail.com>
2024-08-07 10:58:27 +0300

    chore: async db run on launch

--- src/main/java/music/sologram/app/SologramApplication.java
+++ src/main/java/music/sologram/app/SologramApplication.java
@@ -1,22 +1,31 @@
 package music.sologram.app;
 
 import javafx.application.Application;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.stage.Stage;
 import music.sologram.app.data.HibernateConfig;
 import music.sologram.app.windows.installation.InstallationWindow;
 
 import static music.sologram.app.settings.SettingChecker.shouldInstallApp;
-import static music.sologram.app.utils.Utils.getAppLogo;
+import static music.sologram.app.utils.Utils.*;
 
 public class SologramApplication extends Application {
   public static void main(String[] args) {
     launch();
   }
 
+  private final BooleanProperty database = new SimpleBooleanProperty(false);
+
+  private void initDb(){
+    HibernateConfig.init();
+    asyncUiThread(() -> database.set(true));
+  }
+
   @Override
   public void start(Stage stage){
     Runnable runnableStage = () -> {
-      HibernateConfig.init();
+      executeInNewThread(this::initDb);
       stage.setMinWidth(600);
       stage.setMinHeight(500);
       stage.centerOnScreen();
