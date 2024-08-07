a972116d47f020c703efecca06f3db18fc369c20
Abi-Nf <hei.abigail.3@gmail.com>
2024-08-07 08:23:07 +0300

    chore: remove splash made stage based

--- src/main/java/music/sologram/app/SologramApplication.java
+++ src/main/java/music/sologram/app/SologramApplication.java
@@ -4,38 +4,23 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import music.sologram.app.data.HibernateConfig;
 import music.sologram.app.windows.installation.InstallationWindow;
-import music.sologram.app.windows.splash.SplashScreen;
 
-import static music.sologram.app.settings.SettingChecker.isHomeAppOk;
 import static music.sologram.app.settings.SettingChecker.shouldInstallApp;
+import static music.sologram.app.utils.Utils.getAppLogo;
 
 public class SologramApplication extends Application {
   public static void main(String[] args) {
     launch();
   }
 
-  private SplashScreen splash;
-
-  public SologramApplication() {
-    this.splash = new SplashScreen();
-    if(isHomeAppOk()) splash.show();
-  }
-
-  private void removeSplash() {
-    if(splash != null){
-      splash.close();
-      this.splash = null;
-    }
-  }
-
   @Override
   public void start(Stage stage){
     Runnable runnableStage = () -> {
       HibernateConfig.init();
-      removeSplash();
       stage.setMinWidth(600);
       stage.setMinHeight(500);
       stage.centerOnScreen();
+      stage.getIcons().add(getAppLogo());
       stage.show();
     };
     if(shouldInstallApp()){
